98cef2d84160a3d8c8d1203b8127b9cd92707f01
Cassius269 <fahamygaston@gmail.com>
2025-06-26 10:26:09 +0200

    configuration de Vite et mise en place de Boostrap et stylisation

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-const path = require('path');
+import { resolve } from 'path'
 
 export default {
-  root: path.resolve(__dirname, 'src'),
-  server: {
-    port: 8080,
-    hot: true
-  },
+  root: resolve(__dirname, 'src'),
   build: {
-  outDir: path.resolve(__dirname, 'dist'), 
-  emptyOutDir: true                       // <--- Vide le dossier à chaque build
-  }
-};
+    outDir: '../dist'
+  },
+  server: {
+    port: 8080
+  },
+  // Optional: Silence Sass deprecation warnings. See note below.
+  css: {
+     preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            'import',
+            'mixed-decls',
+            'color-functions',
+            'global-builtin',
+          ],
+        },
+     },
+  },
+}
